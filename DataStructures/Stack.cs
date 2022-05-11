b4a93fa98b97eea57b59e99e5df7fa6f3dd055ef
Ameilia Valdes <valdes.ameilia@gmail.com>
2022-05-10 19:22:23 -0700

    added stack class properties and methods

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+  public class Stack<T>
+  {
+    public Node<T> top { get; set; }
+
+    public bool isEmpty()
+    {
+      return top != null;
+    }
+
+    public void Push(T value)
+    {
+      Node<T> node = new Node<T>(value);
+      node.Next = top;
+      top = node;
+    }
+
+    public Node<T> Pop()
+    {
+      try
+      {
+        Node<T> currentTop = top;
+        top = currentTop.Next;
+        return currentTop;
+      }
+      catch
+      {
+        return null;
+      }
+    }
+
+    public Node<T> Peek()
+    {
+
+      return top;
+
+    }
+  }
+}
