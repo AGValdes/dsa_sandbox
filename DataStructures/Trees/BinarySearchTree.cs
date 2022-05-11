025a29e9b6b88ac7658631f6122c239d2469645e
Ameilia Valdes <valdes.ameilia@gmail.com>
2022-05-10 19:47:07 -0700

    added binary tree and binary search tree classes with their respective properties and class methods that demonstrate traversal of each tree

---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+  public class BinarySearchTree<T> : BinaryTree<T> where T : IComparable
+  {
+    public void Add(Node<T> node, T value)
+    {
+      Node<T> newNode = new Node<T>(value);
+
+      if (Root == null)
+      {
+        Root = newNode;
+        return;
+      }
+      if (newNode.Value.CompareTo(node.Value) < 0)
+      {
+        if (node.Left == null) { node.Left = newNode; }
+        else { Add(node.Left, value); }
+      }
+      else if (newNode.Value.CompareTo(node.Value) > 0)
+      {
+        if (node.Right == null) { node.Right = newNode; }
+        else { Add(node.Right, value); }
+      }
+    }
+
+    public static bool Contains(BinarySearchTree<T> tree, T value)
+    {
+      Node<T> node = tree.Root;
+
+      while (node != null)
+      {
+        if (node.Value.Equals(value)) { return true; }
+        else if (node.Value.CompareTo(value) < 0) { node = node.Left; }
+        else { node = node.Right; }
+
+      }
+      return false;
+    }
+  }
+}
