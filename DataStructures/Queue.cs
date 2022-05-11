b2fc5f0955db80b8493c8c537f357f831f65e127
Ameilia Valdes <valdes.ameilia@gmail.com>
2022-05-10 19:17:19 -0700

    added queue class properties and methods

---
+++ DataStructures/Queue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+  public class Queue<T>
+  {
+    public Node<T> Front { get; set; }
+    public Node<T> Rear { get; set; }
+
+
+    public void Enqueue(T value)
+    {
+      Node<T> node = new Node<T>(value);
+      if (Front == null)
+      {
+        Front = node; Rear = node;
+      }
+      Rear.Next = node;
+      Rear = node;
+    }
+
+    public Node<T> Dequeue()
+    {
+      try
+      {
+        Node<T> oldFront = Front;
+        Node<T> tempNode = Front.Next;
+
+        Front = tempNode;
+        return oldFront;
+
+
+      }
+      catch (NullReferenceException nre)
+      {
+        throw nre;
+      }
+    }
+
+    public Node<T> Peek()
+    {
+      if (Front == null)
+      {
+        throw new NullReferenceException("There was nothing in queue!");
+      }
+      return Front;
+
+
+    }
+
+    public bool IsEmpty()
+    {
+      return Front == null;
+    }
+
+
+  }
+}
