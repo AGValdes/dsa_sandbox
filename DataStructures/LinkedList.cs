5039fb6cd6776f6455df607f26ad20d6e5558e61
Ameilia Valdes <valdes.ameilia@gmail.com>
2022-05-10 19:02:21 -0700

    added properties to linked list class

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+  public class LinkedList<T>
+  {
+    public Node<T> Head { get; set; }
+
+    /// <summary>
+    /// Plain, Empty Linked List
+    /// Usage: LinkedLIst myLIst = new LinkedList();
+    /// </summary>
+    public LinkedList() { }
+
+    /// <summary>
+    /// Creates a linked list with a node, assigning Head
+    /// Usage: LinkedList myList = new LinkedList(4);
+    /// </summary>
+    /// <param name="value"></param>
+    public LinkedList(T value)
+    {
+      Node<T> node = new Node<T>(value);
+      Head = node;
+    }
+  }
+}
