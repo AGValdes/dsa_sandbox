87aaacdfea4be12c5f7571dd28af7ff1670a4a7b
Ameilia Valdes <valdes.ameilia@gmail.com>
2022-05-16 16:42:27 -0700

    added logic for TurnStringToCharArray, DetermineOpposites, and GiveBracketsTheirOpposites methods

--- AlgorithmChallenges/Multi_Bracket_Validation.cs
+++ AlgorithmChallenges/Multi_Bracket_Validation.cs
@@ -7,10 +7,12 @@ public class Multi_Bracket_Validation
 {
 	public class Bracket
 	{
+		public static char OGBracket { get; set; }
 		public static char Opposite { get; set; }
 
-		public Bracket(char opposite)
+		public Bracket(char ogbracket, char opposite)
 		{
+			OGBracket = ogbracket;
 			Opposite = opposite;
 		}
 
@@ -19,12 +21,41 @@ public class Multi_Bracket_Validation
 	public static DataStructures.Stack<Bracket> Stack = new DataStructures.Stack<Bracket>();
 	public static char[] TurnStringIntoCharArray(string bracketstring)
 	{
-
+		return bracketstring.ToCharArray();
 	}
-
+	
+	public static char DetermineOpposite(char character)
+	{	
+		if (character == '{')
+		{
+			char opposite = '}';
+			return opposite;
+		}
+		else if (character == '[')
+		{
+			char opposite = ']';
+			return opposite;
+		}
+		else if (character == '(')
+		{
+			char opposite = ')';
+			return opposite;	
+		}
+    else
+		{
+			return '-';
+		}
+	}
 	public static List<Bracket> GiveCharsTheirOpposites(char[] arrayOfChars)
 	{
-
+		List<Bracket> brackets = new List<Bracket>();
+		for (int i = 0; i < arrayOfChars.Length; i++)
+		{
+			Bracket newBracketObj = new Bracket(arrayOfChars[i], DetermineOpposite(arrayOfChars[i]));
+			brackets.Add(newBracketObj);
+		}	
+		return brackets;
+		
 	}
 
 	public static void PutOpeningBracketsInStack(List<Bracket> list)
